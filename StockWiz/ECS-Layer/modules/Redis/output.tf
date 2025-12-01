d87342ac0431544cbc7bd4ee0245f61d566ff3e2
Renzo De Leon <renzodeleonps@gmail.com>
2025-12-01 00:48:56 -0300

    Add output for Redis primary port in the Redis module

--- StockWiz/ECS-Layer/modules/Redis/output.tf
+++ StockWiz/ECS-Layer/modules/Redis/output.tf
@@ -2,3 +2,8 @@ output "redis_primary_endpoint_address" {
   description = "Redis primary endpoint address mapped by environment"
   value       = { for k, rg in aws_elasticache_replication_group.redis : k => rg.primary_endpoint_address }
 }
+
+output "redis_primary_port" {
+  description = "Redis primary endpoint port mapped by environment"
+  value       = { for k, rg in aws_elasticache_replication_group.redis : k => rg.port }
+}
